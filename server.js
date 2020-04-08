e38f8242de0a754f58ebe3b5f4171bb0a2707be7
Konstantin Barkalov <mail@barkalov.ru>
2020-04-08 23:32:25 +0700

    cert

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.set('port', httpsPort);
 const fs = require('fs');
 
 const options = {
-  // key: fs.readFileSync('cert/key.pem'),
-  // cert: fs.readFileSync('cert/cert.pem')
+  key: fs.readFileSync('cert/key.pem'),
+  cert: fs.readFileSync('cert/cert.pem')
 };
 
 const httpServer = http.createServer(app);
